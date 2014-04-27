5b39f7d602cedff3fb86e5a4d75f81586dc89257
Yash Sinha <mail.yash.sinha@gmail.com>
2014-04-27 18:17:56 +0530

    Added functions newNode, lengthIter, lengthRecur.

--- Linked Lists/linkedlist.c
+++ Linked Lists/linkedlist.c
@@ -9,6 +9,13 @@ struct node {
 typedef struct node node;
 typedef node* list;
 
+list newNode(int d) {
+	list node = (list)malloc(sizeof(node));
+	node->next = NULL;
+	node->data = d;
+	return node;
+}
+
 void print(list l) {
 	if(l == NULL)
 		printf("NULL\n");
@@ -22,10 +29,35 @@ void print(list l) {
 	return;
 }
 
+int lengthIter(list l) {
+	list cur = l;
+	int count = 0;
+
+	while(cur != NULL) {
+		count++;
+		cur = cur->next;
+	}
+	return count;
+}
+
+int __lengthRecur(list l, int count) {
+	if(l == NULL)
+		return count;
+	return __lengthRecur(l->next, count+1);
+}
+
+int lengthRecur(list l) {
+	return __lengthRecur(l, 0);
+}
+
 int main(int argc, char const *argv[]) {
-	list l = (list)malloc(sizeof(list));
-	l->next = NULL;
-	l->data = 1;
+	list l = newNode(1);
+	list second = newNode(2);
+	list third = newNode(3);
+	l->next = second;
+	second->next = third;
 	print(l);
+	printf("Count is %d\n", lengthIter(l));
+	printf("Count is %d\n", lengthRecur(l));
 	return 0;
 }
